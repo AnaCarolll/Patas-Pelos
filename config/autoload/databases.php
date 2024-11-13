e945f38df63d63e655431b9c5822c68805c03258
Ana Carolina <anacarolina.silva@onfly.com.br>
2024-11-13 15:12:14 -0300

    modificações na infraestrutura

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -14,11 +14,11 @@ use function Hyperf\Support\env;
 return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST','db'),
+        'host' => env('DB_HOST','localhost'),
         'database' => env('DB_DATABASE', 'patas_e_pelos'),
         'port' => env('DB_PORT', 3306),
-        'username' => env('DB_USERNAME', 'ana'),
-        'password' => env('DB_PASSWORD', 'novasenha'),
+        'username' => env('DB_USERNAME', 'root'),
+        'password' => env('DB_PASSWORD', ''),
         'charset' => env('DB_CHARSET', 'utf8'),
         'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
